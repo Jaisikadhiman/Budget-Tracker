f6884ecb4068902e2c13b59207de130959b0354a
Jaisika Dhiman <jaisikadhiman446@gmail.com>
2025-03-02 14:21:20 +0530

    up

--- client/src/utils/apiURL.js
+++ client/src/utils/apiURL.js
@@ -1,4 +1,4 @@
-// const url = "https://budget-tracker-backend.onrender.com";
-export const API_URL_USER = "http://localhost:9000/api/v1/users";
-export const API_URL_ACCOUNT = "http://localhost:9000/api/v1/accounts";
-export const API_URL_TRANSACTION = "http://localhost:9000/api/v1/transactions";
+// const url = "https://budget-tracker-bmym.onrender.com";
+export const API_URL_USER = "https://budget-tracker-bmym.onrender.com/api/v1/users";
+export const API_URL_ACCOUNT = "https://budget-tracker-bmym.onrender.com/api/v1/accounts";
+export const API_URL_TRANSACTION = "https://budget-tracker-bmym.onrender.com/api/v1/transactions";
